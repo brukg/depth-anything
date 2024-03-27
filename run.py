7c50104db289b6773501fb64f572ed8128f4d3ba
brukg <bruk@signaltech.xyz>
2024-03-27 10:49:04 +0100

    added camera capture

--- run.py
+++ run.py
@@ -11,30 +11,49 @@ from depth_anything.dpt import DepthAnything
 from depth_anything.util.transform import Resize, NormalizeImage, PrepareForNet
 
 
+def process_image(image):
+    h, w = image.shape[:2]
+
+    image = transform({'image': image})['image']
+    image = torch.from_numpy(image).unsqueeze(0).to(DEVICE)
+
+    with torch.no_grad():
+        depth = depth_anything(image)
+
+    depth = F.interpolate(depth[None], (h, w), mode='bilinear', align_corners=False)[0, 0]
+    depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
+
+    depth = depth.cpu().numpy().astype(np.uint8)
+    depth_color = cv2.applyColorMap(depth, cv2.COLORMAP_INFERNO)
+
+    return depth_color
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--img-path', type=str)
     parser.add_argument('--outdir', type=str, default='./vis_depth')
-    parser.add_argument('--encoder', type=str, default='vitl', choices=['vits', 'vitb', 'vitl'])
-    
+    parser.add_argument('--encoder', type=str, default='vits', choices=['vits', 'vitb', 'vitl'])
+    parser.add_argument('--camera', action='store_true')  # Add camera option
+
     args = parser.parse_args()
-    
+
     margin_width = 50
     caption_height = 60
-    
+
     font = cv2.FONT_HERSHEY_SIMPLEX
     font_scale = 1
     font_thickness = 2
-    
+
     DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-    
+
     depth_anything = DepthAnything.from_pretrained('LiheYoung/depth_anything_{}14'.format(args.encoder)).to(DEVICE)
-    
+
     total_params = sum(param.numel() for param in depth_anything.parameters())
     print('Total parameters: {:.2f}M'.format(total_params / 1e6))
-    
+
     depth_anything.eval()
-    
+
     transform = Compose([
         Resize(
             width=518,
@@ -48,55 +67,85 @@ if __name__ == '__main__':
         NormalizeImage(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         PrepareForNet(),
     ])
-    
-    if os.path.isfile(args.img_path):
-        if args.img_path.endswith('txt'):
-            with open(args.img_path, 'r') as f:
-                filenames = f.read().splitlines()
-        else:
-            filenames = [args.img_path]
+
+    if args.camera:
+        cap = cv2.VideoCapture(0)
+        frame_count = 0
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            frame_count += 1
+            if frame_count % 2 == 0:
+                continue
+
+            depth_color = process_image(frame)
+
+            os.makedirs(args.outdir, exist_ok=True)
+            filename = 'camera_frame_img_depth.png'
+
+            split_region = np.ones((frame.shape[0], margin_width, 3), dtype=np.uint8) * 255
+            combined_results = cv2.hconcat([frame, split_region, depth_color])
+
+            caption_space = np.ones((caption_height, combined_results.shape[1], 3), dtype=np.uint8) * 255
+            captions = ['Raw image', 'Depth Anything']
+            segment_width = frame.shape[1] + margin_width
+            for i, caption in enumerate(captions):
+                # Calculate text size
+                text_size = cv2.getTextSize(caption, font, font_scale, font_thickness)[0]
+
+                # Calculate x-coordinate to center the text
+                text_x = int((segment_width * i) + (frame.shape[1] - text_size[0]) / 2)
+
+                # Add text caption
+                cv2.putText(caption_space, caption, (text_x, 40), font, font_scale, (0, 0, 0), font_thickness)
+
+            final_result = cv2.vconcat([caption_space, combined_results])
+
+            cv2.imshow('Depth Anything', final_result)
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
     else:
-        filenames = os.listdir(args.img_path)
-        filenames = [os.path.join(args.img_path, filename) for filename in filenames]
-        filenames.sort()
-    
-    for filename in tqdm(filenames):
-        raw_image = cv2.imread(filename)
-        image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2RGB) / 255.0
-        
-        h, w = image.shape[:2]
-        
-        image = transform({'image': image})['image']
-        image = torch.from_numpy(image).unsqueeze(0).to(DEVICE)
-        
-        with torch.no_grad():
-            depth = depth_anything(image)
-        
-        depth = F.interpolate(depth[None], (h, w), mode='bilinear', align_corners=False)[0, 0]
-        depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
-        
-        depth = depth.cpu().numpy().astype(np.uint8)
-        depth_color = cv2.applyColorMap(depth, cv2.COLORMAP_INFERNO)
-        
-        os.makedirs(args.outdir, exist_ok=True)
-        filename = os.path.basename(filename)
-        
-        split_region = np.ones((raw_image.shape[0], margin_width, 3), dtype=np.uint8) * 255
-        combined_results = cv2.hconcat([raw_image, split_region, depth_color])
-        
-        caption_space = np.ones((caption_height, combined_results.shape[1], 3), dtype=np.uint8) * 255
-        captions = ['Raw image', 'Depth Anything']
-        segment_width = w + margin_width
-        for i, caption in enumerate(captions):
-            # Calculate text size
-            text_size = cv2.getTextSize(caption, font, font_scale, font_thickness)[0]
+        if os.path.isfile(args.img_path):
+            if args.img_path.endswith('txt'):
+                with open(args.img_path, 'r') as f:
+                    filenames = f.read().splitlines()
+            else:
+                filenames = [args.img_path]
+        else:
+            filenames = os.listdir(args.img_path)
+            filenames = [os.path.join(args.img_path, filename) for filename in filenames]
+            filenames.sort()
 
-            # Calculate x-coordinate to center the text
-            text_x = int((segment_width * i) + (w - text_size[0]) / 2)
+        for filename in tqdm(filenames):
+            raw_image = cv2.imread(filename)
+            image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2RGB) / 255.0
 
-            # Add text caption
-            cv2.putText(caption_space, caption, (text_x, 40), font, font_scale, (0, 0, 0), font_thickness)
-        
-        final_result = cv2.vconcat([caption_space, combined_results])
-        
-        cv2.imwrite(os.path.join(args.outdir, filename[:filename.find('.')] + '_img_depth.png'), final_result)
+            depth_color = process_image(image)
+
+            os.makedirs(args.outdir, exist_ok=True)
+            filename = os.path.basename(filename)
+
+            split_region = np.ones((raw_image.shape[0], margin_width, 3), dtype=np.uint8) * 255
+            combined_results = cv2.hconcat([raw_image, split_region, depth_color])
+
+            caption_space = np.ones((caption_height, combined_results.shape[1], 3), dtype=np.uint8) * 255
+            captions = ['Raw image', 'Depth Anything']
+            segment_width = raw_image.shape[1] + margin_width
+            for i, caption in enumerate(captions):
+                # Calculate text size
+                text_size = cv2.getTextSize(caption, font, font_scale, font_thickness)[0]
+
+                # Calculate x-coordinate to center the text
+                text_x = int((segment_width * i) + (raw_image.shape[1] - text_size[0]) / 2)
+
+                # Add text caption
+                cv2.putText(caption_space, caption, (text_x, 40), font, font_scale, (0, 0, 0), font_thickness)
+
+            final_result = cv2.vconcat([caption_space, combined_results])
+
+            cv2.imwrite(os.path.join(args.outdir, filename[:filename.find('.')] + '_img_depth.png'), final_result)
